ca93b8b0a07d82d93370499d638b35be679c15b9
Domingo Suarez Torres <user@GEMs-MacBook-Pro-3.local>
2009-04-25 01:13:37 -0500

    initial load

---
+++ code/HolaMundo.groovy
@@ -0,0 +1,15 @@
+/*
+ * En Groovy no hay necesidad de escribir un metodo main para ejecutar un clase.
+ */
+
+println 'Hola mundo'
+
+/*
+ * ¿Como le hago para acceder a los parametros que la JVM pasa a la clase?
+ */
+
+println "Parametros recibidos $args"
+
+/*
+ * Observen la diferencia en las comillas simple y dobles
+ */
